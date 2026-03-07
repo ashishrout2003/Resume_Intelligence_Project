c0fca6daff6aa7c8208d86fd07327bf1bcfb902d
Ashish Rout <ashishrout356@gmail.com>
2026-03-07 23:31:10 +0530

    initial commit

---
+++ Server.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const app = require('./Src/App')
+const connectDB = require('./Src/Config/Database')
+
+connectDB()
+
+app.listen(3000, ()=>{
+    console.log("Server is running on port 3000");
+    
+})
